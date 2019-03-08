351a49280d7f298230a7675371fd8459858dca16
Linus Phan <phanlinus@gmail.com>
2019-03-08 10:31:37 -0800

    Set up backend for updating list title
    Co-authored-by: Mat Sachs <matochondrion@gmail.com>
    Co-authored-by: Linus Phan <phanlinus@gmail.com>
    Co-authored-by: Jacob Coker <jacobleecd@gmail.com>

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,8 +1,35 @@
 class Api::ListsController < ApplicationController
-  def update
-    
+  def create
+    @board = Board.find(params[:board_id])
+    @list = List.new(list_params.merge({ board_id: @board.id }))
+  
+    if @list.save
+      render :create, status: :created
+    else
+      @error = @list.errors.full_messages.join(', ')
+      render 'api/shared/error', status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+    @error = 'Something went wrong'
+    render 'api/shared/error', status: :not_found
   end
 
-  def create
+  def update
+    @list = List.find(params[:id])
+    @list.title = params[:title]
+    # @list.position = params[:position]
+
+    @list.save
+
+    render :update, status: :ok
+  rescue
+    @error = 'Something went wrong'
+    render 'api/shared/error', status: :unprocessable_entity
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title)
   end
 end
