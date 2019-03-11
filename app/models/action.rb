d3ad0d7098bfa12f4627cd09605333650152eb18
Jacob Coker-Dukowitz <jacobleecd@gmail.com>
2019-03-11 11:15:16 -0700

    add feild_to_description in actions
    Co-authored-by: Mat Sachs <matochondrion@gmail.com>
    Co-authored-by: Linus Phan <phanlinus@gmail.com>
    Co-authored-by: Jacob Coker <jacobleecd@gmail.com>

--- app/models/action.rb
+++ app/models/action.rb
@@ -2,6 +2,16 @@ class Action < ApplicationRecord
   validates_presence_of :description, allow_blank: false
   belongs_to :card, foreign_key: 'card_id', class_name: 'Card', required: true
 
-# controller: if has any of list of keys
-  #
+  def field_to_description(field, content)
+    {
+      title: " Changed title to #{content}",
+      list_id: " Changed list to #{List.find(content.to_i).title}",
+      position: " Changed position to #{content}",
+      description: " Changed description to #{content}",
+      archived: " Changed archived to #{content}",
+      due_date: " Changed due date to #{content}",
+      completed: " Marked completed as #{content}",
+      labels: " Added labels #{content}"
+    }
+  end
 end
