b704dad097727372c2552c4b62c47b664202016f
Linus Phan <phanlinus@gmail.com>
2019-03-18 00:51:11 -0700

    setting up due date popover behavior

---
+++ app/javascript/components/board/DueDateForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class DueDateForm extends React.Component {
+  render () {
+    console.log('Due Date form should be rendered!');
+  }
+}
+
+export default DueDateForm;
--- app/javascript/components/board/SingleCardContainer.jsx
+++ app/javascript/components/board/SingleCardContainer.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import * as actions from '../../actions/BoardActions';
 import SingleCard from './SingleCard';
 import Popover from './Popover';
+import DueDateForm from './DueDateForm';
 
 export default class SingleCardContainer extends React.Component {
   state = {
@@ -16,6 +17,10 @@ export default class SingleCardContainer extends React.Component {
   popoverChildren = () => {
     if (this.state.popover.visible && this.state.popover.type) {
       switch (this.state.popover.type) {
+        case 'due-date':
+          return (
+            <DueDateForm />
+          );
       }
     }
   };
